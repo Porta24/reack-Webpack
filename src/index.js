778adb573fdadd43d470a97f262ee56d720f5c72
Porta24 <marporta24@hotmail.com>
2021-07-30 09:48:34 -0600

    initial webpack setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const aztecSoup = {
+    tomatos: 8,
+    onions: 6,
+    garlic: 6,
+    celery: 1,
+    coriander: 2,
+    avocado: 3,
+    chips: 2,
+    cheese: 2,
+}
+
+console.log(aztecSoup);
